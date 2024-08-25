a6e85cebe453a71630c8c6b83d9c711a51cf0417
lnfidalgo <lucasfidalgo07@gmail.com>
2024-08-25 11:11:10 -0300

    teste de Link com ID em producao

--- src/app/carrinho/page.tsx
+++ src/app/carrinho/page.tsx
@@ -1,7 +1,40 @@
+"use client";
+import { Button } from "@/components/ui/button";
+import { usePathname, useSearchParams } from "next/navigation";
+
 export default function Carrinho() {
+  const searchParams = useSearchParams();
+
+  function updateSorting(sortOrder: string) {
+    const params = new URLSearchParams(searchParams.toString());
+    params.set("sort", sortOrder);
+    window.history.pushState(null, "", `?${params.toString()}`);
+  }
+
+  const pathname = usePathname();
+
+  function switchLocale(locale: string) {
+    // e.g. '/en/about' or '/fr/contact'
+    const newPath = `/${locale}${pathname}`;
+    window.history.replaceState(null, "", newPath);
+  }
+
   return (
-    <>
-      <div>Carrinho</div>
-    </>
+    <div>
+      <div className="h-[900px]">
+        <h2>Carrinho</h2>
+      </div>
+      <div className="h-[900px]">
+        <Button onClick={() => updateSorting("asc")}>Sort Ascending</Button>
+        <Button onClick={() => updateSorting("desc")}>Sort Descending</Button>
+      </div>
+      <div className="h-[900px]">
+        <Button onClick={() => switchLocale("en")}>English</Button>
+        <Button onClick={() => switchLocale("fr")}>French</Button>
+      </div>
+      <div className="h-[900px]">
+        <h2 id="testeID">Teste de rota com ID</h2>
+      </div>
+    </div>
   );
 }
--- src/ui/screen/MainScreen.tsx
+++ src/ui/screen/MainScreen.tsx
@@ -1,7 +1,13 @@
+import { Button } from "@/components/ui/button";
+import Link from "next/link";
+
 export default function MainScreen() {
   return (
     <div className="w-full bg-red-100">
       <h1>Home</h1>
+      <Link href={"/carrinho#testeID"}>
+        <Button>Teste ID</Button>
+      </Link>
     </div>
   );
 }
